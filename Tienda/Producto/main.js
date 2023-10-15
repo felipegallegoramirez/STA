7903cf066eced9359deccd93d38d611b021d8c4e
Felipe Gallego <felipegallego600@gmail.com>
2023-10-15 17:22:23 -0500

    Product-Noresponsive

---
+++ Tienda/Producto/main.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+showSlidesA(slideIndex);
+let slidesCount = document.getElementsByClassName("mySlides").length;
+
+// Next/previous controls
+function plusSlides(n) {
+  slideIndex += n;
+
+  if (n > slidesCount-1) {slideIndex = 1}
+  if (n < 1) {slideIndex = slidesCount}
+  showSlides(slideIndex) 
+}
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (slideIndex > slidesCount-1) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slidesCount}
+    slides[slideIndex-1].style.display = "block";
+  } 
+
+
+function getSlide(){
+    return slideIndex
+}
+
+function showSlidesA() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex=getSlide()
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlidesA, 4000); // Change image every 2 seconds
+  } 
